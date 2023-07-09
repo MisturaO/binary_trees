ae17384d3fac3599510295b9e106902f4d264f08
MisturaO <misturaojulari91@gmail.com>
2023-07-09 16:31:39 +0100

    task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,7 +4,7 @@
  * binary_tree_is_root - checks if a node is a root
  * @node: node to check
  *
- * Return: 1 if node is a root
+ * Return: 1 iff node is a root
  *         0 if not a root
  *         0 if node is NULL
  */
